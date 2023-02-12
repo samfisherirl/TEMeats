93c956adce1f1a3a8c48c8d82ceae3c72f26aea0
Sasha Alexander <98753696+samfisherirl@users.noreply.github.com>
2023-02-12 13:09:50 -0600

    xxx

--- temeats.com/wp-content/cache/min/1/wp-content/themes/enfold/config-templatebuilder/avia-shortcodes/chart/chartf08d.js
+++
@@ -1,3 +0,0 @@
-(function($){"use strict";$.fn.aviaChart=function(options){if(!this.length){return}
-return this.each(function(){var container=$(this),config=container.data('chart_config'),context=container.find('.avia-chart').first(),chart=null;if(!context.length||'undefined'==typeof(config)){return}
-chart=new Chart(context,config)})};$(function(){$('.avia-chart-container').aviaChart()})}(jQuery))
